80998fcbf096a722f34dace89c7f0dd2e4311c82
unknown <s4shreyadhingra@gmail.com>
2023-11-13 23:49:46 +0530

    added dropdown menu

--- src/components/header/Menu.jsx
+++ src/components/header/Menu.jsx
@@ -1,14 +1,28 @@
-import styles from './Header.module.css';
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
 
 const Menu = () => {
   return (
     <nav>
-        <ul className={`${styles.menu} ${styles["menu-navbar"]}`}>
-            <li>Subjects</li>
-            <li>Courses</li>
-            <li>About Us</li>
-        </ul>
+      <ul className={`${styles.menu} ${styles["menu-navbar"]}`}>
+        <li className={styles.dropdown}>
+          <button className={styles.dropbtn}>Subjects</button>
+          <div className={styles["dropdown-content"]}>
+            <Link to={"/mathematics"}>Mathematics</Link>
+            <Link to={"/physics"}>Physics</Link>
+            <Link to={"/chemistry"}>Chemistry</Link>
+            <Link to={"/sanskrit"}>Sanskrit</Link>
+            <Link to={"/english"}>English</Link>
+            <Link to={"/hindi"}>Hindi</Link>
+          </div>
+        </li>
+        <li>Courses</li>
+        <li>About Us</li>
+        <li>
+          <Link to={"/quiz"}>Quiz</Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
-export default Menu
+  );
+};
+export default Menu;
