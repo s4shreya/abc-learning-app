fdd74acd0435b01f47e631258177b97225a36c45
unknown <s4shreyadhingra@gmail.com>
2023-11-16 23:56:34 +0530

    added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import styles from "./App.module.css";
-import MainContent from "./components/main-content/MainContent";
 import Footer from "./components/Footer";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <div className={styles.container}>
-      {/* <MainContent /> */}
+      <Homepage />
       <Footer />
     </div>
   );
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,8 @@
+import styles from './Homepage.module.css'
+
+const Homepage = () => {
+  return (
+    <div className={styles.circle}></div>
+  )
+}
+export default Homepage
