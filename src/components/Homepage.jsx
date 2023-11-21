6131727d2d869523ce3024b8bdc08883f27aaf81
unknown <s4shreyadhingra@gmail.com>
2023-11-21 17:25:24 +0530

    added course categories component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,13 +1,19 @@
 import styles from "./Homepage.module.css";
+import CourseCategories from "./homepage/CourseCategories";
 
 const Homepage = () => {
   const circles = ["I-V", "VI-VIII", "IX-X", "XI-XII"];
 
   return (
-    <div className={styles["circle-container"]}>
-      {circles.map((circle,index) => (
-        <div key={index} className={styles.circle}><p>{circle}</p></div>
-      ))}
+    <div>
+      <CourseCategories />
+      <div className={styles["circle-container"]}>
+        {circles.map((circle, index) => (
+          <div key={index} className={styles.circle}>
+            <p>{circle}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/homepage/CourseCategories.jsx
@@ -0,0 +1,68 @@
+import styles from "./CourseCategories.module.css";
+
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+import HtmlIcon from "@mui/icons-material/Html";
+import IntegrationInstructionsIcon from "@mui/icons-material/IntegrationInstructions";
+import CodeIcon from "@mui/icons-material/Code";
+import ApiIcon from "@mui/icons-material/Api";
+import WebhookIcon from "@mui/icons-material/Webhook";
+import ScoreIcon from "@mui/icons-material/Score";
+import AddBusinessIcon from "@mui/icons-material/AddBusiness";
+import WorkHistoryIcon from "@mui/icons-material/WorkHistory";
+
+const CourseCategories = () => {
+  const courses = [
+    {
+      name: "Web Development",
+      icon: <HtmlIcon />,
+    },
+    {
+      name: "Mobile Development",
+      icon: <DeveloperModeIcon />,
+    },
+    {
+      name: "Machine Learning",
+      icon: <CodeIcon />,
+    },
+    {
+      name: "Data Science",
+      icon: <ScoreIcon />,
+    },
+    {
+      name: "Digital Marketing",
+      icon: <AddBusinessIcon />,
+    },
+    {
+      name: "Business Management",
+      icon: <WorkHistoryIcon />,
+    },
+    {
+      name: "DevOps",
+      icon: <IntegrationInstructionsIcon />,
+    },
+    {
+      name: "Computer Vision",
+      icon: <WebhookIcon />,
+    },
+    {
+      name: "Artificial Intelligence",
+      icon: <ApiIcon />,
+    },
+  ];
+
+  return (
+    <div className={styles.container}>
+      <h3>COURSE CATEGORIES</h3>
+      <h5>Popular topics to learn</h5>
+      <div className={styles["course-container"]}>
+        {courses.map((course, index) => (
+          <p key={index} className={styles.items}>
+            {course.icon}
+            {course.name}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default CourseCategories;
